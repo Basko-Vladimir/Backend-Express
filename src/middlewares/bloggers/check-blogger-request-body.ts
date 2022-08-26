ab765f810a050ddfb7578915002b6693108807c3
Basko Vladimir <1989bvg@gmail.com>
2022-08-26 17:11:50 +0300

    removed .bail()

--- src/middlewares/bloggers/check-blogger-request-body.ts
+++ src/middlewares/bloggers/check-blogger-request-body.ts
@@ -2,10 +2,10 @@ import { body } from "express-validator";
 
 export const checkBloggerRequestBody = [
 	body("name")
-		.exists().bail().withMessage("You didn't provide 'name' field")
+		.exists().withMessage("You didn't provide 'name' field")
 		.isLength({max: 15}).withMessage("Name should be less than 15 chars"),
 	body("youtubeUrl")
-		.exists().bail().withMessage("You didn't provide 'youtubeUrl' field")
-		.isURL().bail().withMessage("URL doesn't match to pattern '^https://([a-zA-Z0-9_-]+\\.)+[a-zA-Z0-9_-]+(\\/[a-zA-Z0-9_-]+)*\\/?$'",)
+		.exists().withMessage("You didn't provide 'youtubeUrl' field")
+		.isURL().withMessage("URL doesn't match to pattern '^https://([a-zA-Z0-9_-]+\\.)+[a-zA-Z0-9_-]+(\\/[a-zA-Z0-9_-]+)*\\/?$'",)
 		.isLength({max: 100}).withMessage("Name should be less than 100 chars")
 ];

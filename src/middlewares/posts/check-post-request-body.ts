f4397ab63d1a6b3d1bebbb16662598f32bfe9156
Basko Vladimir <1989bvg@gmail.com>
2022-08-26 18:50:32 +0300

    added .trim() before .exists()

--- src/middlewares/posts/check-post-request-body.ts
+++ src/middlewares/posts/check-post-request-body.ts
@@ -3,16 +3,16 @@ import {bloggersRepository} from "../../repositories/bloggers-repository";
 
 export const checkPostRequestBody = [
 	body("title")
-		.exists().withMessage("You didn't provide 'title' field")
+		.trim().exists().withMessage("You didn't provide 'title' field")
 		.isLength({max: 30}).withMessage("Name should be less than 30 chars"),
 	body("shortDescription")
-		.exists().withMessage("You didn't provide 'shortDescription' field")
+		.trim().exists().withMessage("You didn't provide 'shortDescription' field")
 		.isLength({max: 100}).withMessage("Name should be less than 100 chars"),
 	body("content")
-		.exists().withMessage("You didn't provide 'content' field")
+		.trim().exists().withMessage("You didn't provide 'content' field")
 		.isLength({max: 1000}).withMessage("Name should be less than 1000 chars"),
 	body("bloggerId")
-		.exists().withMessage("You didn't provide 'bloggerId' field")
+		.trim().exists().withMessage("You didn't provide 'bloggerId' field")
 		.custom(value => {
 			 if (!bloggersRepository.getBloggerById(value)) {
 				 throw new Error(`Blogger with id "${value}" does not exist`);

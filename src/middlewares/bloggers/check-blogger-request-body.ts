6d58888b3826792cae428fcb9c7e5552971c2e17
Basko Vladimir <1989bvg@gmail.com>
2022-08-26 19:09:01 +0300

    added trimand min length

--- src/middlewares/bloggers/check-blogger-request-body.ts
+++ src/middlewares/bloggers/check-blogger-request-body.ts
@@ -3,9 +3,9 @@ import { body } from "express-validator";
 export const checkBloggerRequestBody = [
 	body("name")
 		.exists().withMessage("You didn't provide 'name' field")
-		.isLength({max: 15}).withMessage("Name should be less than 15 chars"),
+		.trim().isLength({min: 1, max: 15}).withMessage("Name should be from 1 to 15 chars"),
 	body("youtubeUrl")
 		.exists().withMessage("You didn't provide 'youtubeUrl' field")
+		.trim().isLength({min: 1, max: 100}).withMessage("Name should be from 1 to 100 chars")
 		.isURL().withMessage("URL doesn't match to pattern '^https://([a-zA-Z0-9_-]+\\.)+[a-zA-Z0-9_-]+(\\/[a-zA-Z0-9_-]+)*\\/?$'",)
-		.isLength({max: 100}).withMessage("Name should be less than 100 chars")
 ];

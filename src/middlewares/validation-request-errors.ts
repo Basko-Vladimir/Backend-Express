31b8e90622b795d9f369aebb5eda69f34b4ef4dc
Basko Vladimir <1989bvg@gmail.com>
2022-08-26 18:44:23 +0300

    added setting onlyFirstError: true

--- src/middlewares/validation-request-errors.ts
+++ src/middlewares/validation-request-errors.ts
@@ -6,7 +6,7 @@ export const validationRequestErrors = (req: Request, res: Response, next: NextF
 	const errors = validationResult(req).formatWith(errorFormatter);
 	
 	if (!errors.isEmpty()) {
-		res.status(400).json({ errorsMessages: errors.array() });
+		res.status(400).json({ errorsMessages: errors.array({onlyFirstError: true}) });
 	} else {
 		next();
 	}

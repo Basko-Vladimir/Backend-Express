91b1546f87068ee885d230ed7cbf2ca1e9d82323
Basko Vladimir <1989bvg@gmail.com>
2022-09-03 11:48:07 +0300

    deleted message on delete all data

--- src/routers/testing-router.ts
+++ src/routers/testing-router.ts
@@ -5,5 +5,5 @@ export const testingRouter = Router({});
 
 testingRouter.delete("/all-data", async (req: Request, res: Response) => {
 	await testingRepository.deleteAllData();
-	res.status(204).send("All data is deleted");
+	res.status(204);
 });

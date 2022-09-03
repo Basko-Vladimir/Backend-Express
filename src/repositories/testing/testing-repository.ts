141fffe6d9c83af600e2a97a80a5cc27c58df13e
Basko Vladimir <1989bvg@gmail.com>
2022-09-03 14:05:15 +0300

    added link to atlas db

--- src/repositories/testing/testing-repository.ts
+++ src/repositories/testing/testing-repository.ts
@@ -1,10 +1,8 @@
 import { postsRepository } from "../posts/db-posts-repository";
 import { bloggersRepository } from "../bloggers/db-bloggers-repository";
-// import { DeleteResult } from "mongodb";
 
 export const testingRepository = {
 	async deleteAllData(): Promise<void> {
-		// return Promise.all([bloggersRepository.deleteAllBloggers(), postsRepository.deleteAllPosts()]);
 		await bloggersRepository.deleteAllBloggers();
 		await postsRepository.deleteAllPosts();
 	}
--- src/routers/testing-router.ts
+++ src/routers/testing-router.ts
@@ -1,9 +1,9 @@
 import { Request, Response, Router } from "express";
-import { testingRepository } from "../repositories/testing/memory-testing-repository";
+import { testingRepository } from "../repositories/testing/testing-repository";
 
 export const testingRouter = Router({});
 
 testingRouter.delete("/all-data", async (req: Request, res: Response) => {
 	await testingRepository.deleteAllData();
-	return res.sendStatus(204);
+	res.sendStatus(204);
 });

094c60daa3c34d0db0ce27fbb39a1347f5cd62d5
Basko Vladimir <1989bvg@gmail.com>
2022-09-15 10:16:19 +0300

    fix: fixed middleware logic of blogId checking

--- src/middlewares/blogs/blog-id-param-validation.ts
+++ src/middlewares/blogs/blog-id-param-validation.ts
@@ -1,10 +1,17 @@
 import {Request, Response, NextFunction} from "express";
 import {ParamBlogIdInputModel} from "../../models/blogs/input-models";
+import {blogsService} from "../../services/blogs-service";
 
-export const blogIdParamValidation = (
+export const blogIdParamValidation = async (
 	req: Request<ParamBlogIdInputModel, {}, {}, {}>,
 	res: Response,
 	next: NextFunction
 ) => {
-	req.params.blogId ? next() : res.sendStatus(404);
+	if (!req.params.blogId) {
+		res.sendStatus(404);
+		return;
+	}
+	
+	const blog = await blogsService.getBlogById(req.params.blogId);
+	blog ? next() : res.sendStatus(404);
 };

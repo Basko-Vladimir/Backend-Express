92ce3c63ca1d4edecb21a097a85beb1b3eeb8885
Basko Vladimir <1989bvg@gmail.com>
2022-09-28 14:48:55 +0300

    adjusted errors handling upon validation of confirmation code

--- src/middlewares/auth/confirmation-code-validation.ts
+++ src/middlewares/auth/confirmation-code-validation.ts
@@ -5,11 +5,13 @@ export const confirmationCodeValidation = body("code")
 	.exists().withMessage("You didn't provide confirmation code!")
 	.custom(async (code, meta: Meta) => {
 		const user = await usersRepository.getUserByFilter({confirmationCode: code});
-		const isMatchedCode = user && user.emailConfirmation.confirmationCode === code;
 		
-		// if (isMatchedCode && user.emailConfirmation.isConfirmed) throw new Error("Confirmation code is confirmed already!");
-		if (!isMatchedCode) throw new Error("Confirmation code is not valid!");
+		if (user) {
+			if (user.emailConfirmation.isConfirmed) throw new Error("Confirmation code is confirmed already!");
+			if (user.emailConfirmation.confirmationCode !== code) throw new Error("Confirmation code is not valid!");
+			
+			meta.req.user = user;
+		}
 		
-		meta.req.user = user;
 		return code;
 	});

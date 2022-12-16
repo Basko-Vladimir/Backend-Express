e5b26e90dad314b6bf59329958e94418e8e72370
Basko Vladimir <1989bvg@gmail.com>
2022-12-16 14:20:32 +0100

    fix in clientRequestsCountValidation middleware

--- src/middlewares/auth/client-requests-count-validation.ts
+++ src/middlewares/auth/client-requests-count-validation.ts
@@ -15,19 +15,19 @@ export const clientRequestsCountValidation = async (req: Request, res: Response,
 	const sortFilter = {[ClientRequestSortByField.createTimeStamp as string]: DbSortDirection.ASC};
 	const clientRequests = await clientRequestsService.getClientRequestsByFilter({endpoint, ip}, sortFilter);
 	
-	if (clientRequests.length > COUNT_LIMIT) {
+	if (clientRequests.length >= COUNT_LIMIT) {
 		const timeBetweenLastFirstRequests =
 			clientRequests[clientRequests.length - 1].createTimeStamp - clientRequests[0].createTimeStamp;
-		
-		await clientRequestsService.updateClientRequest(
-			String(clientRequests[0]._id),
-			{[ClientRequestSortByField.createTimeStamp]: Date.now()}
-		);
 		
 		if (timeBetweenLastFirstRequests <= TIME_LIMIT) {
 			res.sendStatus(429);
 			return;
 		}
+		
+		await clientRequestsService.updateClientRequest(
+			String(clientRequests[0]._id),
+			{[ClientRequestSortByField.createTimeStamp]: Date.now()}
+		);
 	} else {
 		await clientRequestsService.createClientRequest(endpoint, ip);
 	}

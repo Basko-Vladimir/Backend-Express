3ae9978d7e6428af7493893faa69f7b3a689d9dd
Vladimir Basko <1989bvg@gmail.com>
2023-01-04 19:51:41 +0100

    added LikeStatus sanitanization

--- src/middlewares/comments/likes-input-data-validation.ts
+++ src/middlewares/comments/likes-input-data-validation.ts
@@ -1,18 +1,21 @@
 import {body} from "express-validator";
 import {generateMissedPropError} from "../../common/error-messages";
 import {LikeStatus} from "../../common/enums";
+import {makeCapitalizeString} from "../../common/utils";
 
 export const likesInputDataValidation = body("likeStatus")
 	.exists().withMessage(generateMissedPropError("likeStatus"))
-	.toLowerCase()
+	.customSanitizer(value => makeCapitalizeString(value))
 	.custom(likeStatus => {
+		const handledLikeStatus = makeCapitalizeString(likeStatus)
+		
 		const isCorrectLikeStatus = [
-			LikeStatus.NONE === likeStatus,
-			LikeStatus.DISLIKE === likeStatus,
-			LikeStatus.LIKE === likeStatus,
+			LikeStatus.NONE === handledLikeStatus,
+			LikeStatus.DISLIKE === handledLikeStatus,
+			LikeStatus.LIKE === handledLikeStatus,
 		].some(item => item);
 		
-		if (isCorrectLikeStatus) return likeStatus;
+		if (isCorrectLikeStatus) return handledLikeStatus;
 		
 		throw new Error("Incorrect value of enum field 'likeStatus'");
 	});

4772c9ef825b03c856167a2f04eb3361d36d2691
Vladimir Basko <1989bvg@gmail.com>
2023-01-19 13:46:06 +0100

    removed unused interfaces

--- src/infrastructure/repositories/interfaces/users-interfaces.ts
+++ src/infrastructure/repositories/interfaces/users-interfaces.ts
@@ -1,18 +1,3 @@
-import {ObjectId} from "mongodb";
-import { IEmailConfirmation } from "../../../domain/users/UserTypes";
-
-export interface DbUser {
-	_id: ObjectId;
-	login: string;
-	email: string;
-	passwordSalt: string;
-	passwordHash: string;
-	passwordRecoveryCode: string;
-	emailConfirmation: IEmailConfirmation;
-	createdAt: Date;
-	__v: number;
-}
-
 export interface UserFilter {
 	email?: string;
 	passwordHash?: string;

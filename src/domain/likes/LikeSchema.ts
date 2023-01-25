50cb023c44029e1468c2c1cacca003d9a8bba20a
Vladimir Basko <1989bvg@gmail.com>
2023-01-25 12:52:04 +0100

    added automatically timestamps for likes

--- src/domain/likes/LikeSchema.ts
+++ src/domain/likes/LikeSchema.ts
@@ -2,33 +2,32 @@ import {Schema, Types} from "mongoose";
 import {LikeStatus} from "../../common/enums";
 import {ILike, ILikeModel, LikeModel} from "./LikeTypes";
 
-export const likeSchema = new Schema<ILike, {}, ILikeModel>({
-	userId: {
-		type: Types.ObjectId,
-		required: true
+export const likeSchema = new Schema<ILike, {}, ILikeModel>(
+	{
+		userId: {
+			type: Types.ObjectId,
+			required: true
+		},
+		userLogin: {
+			type: String,
+			required: true
+		},
+		commentId: {
+			type: Types.ObjectId,
+			default: null
+		},
+		postId: {
+			type: Types.ObjectId,
+			required: true
+		},
+		status: {
+			type: String,
+			enum: [LikeStatus.LIKE, LikeStatus.DISLIKE, LikeStatus.NONE],
+			required: true
+		}
 	},
-	userLogin: {
-		type: String,
-		required: true
-	},
-	commentId: {
-		type: Types.ObjectId,
-		default: null
-	},
-	postId: {
-		type: Types.ObjectId,
-		required: true
-	},
-	status: {
-		type: String,
-		enum: [LikeStatus.LIKE, LikeStatus.DISLIKE, LikeStatus.NONE],
-		required: true
-	},
-	createdAt: {
-		type: Date,
-		default: new Date()
-	}
-});
+	{timestamps: true}
+);
 
 likeSchema.method("updateLikeStatus", function (status: LikeStatus): ILike {
 	const that = this as ILike;
